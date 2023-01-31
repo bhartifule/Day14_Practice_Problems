ae7e65eec85871565683688aed24b4bd75d2679e
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-01-31 13:45:22 +0530

    Added :StackPush Operations

--- bridgelabz/stacksandqueues/StacksAndQueues.java
+++ bridgelabz/stacksandqueues/StacksAndQueues.java
@@ -3,7 +3,7 @@ package com.bridgelabz.stacksandqueues;
 public class StacksAndQueues{
 	 public static void main(String[] args) {
 
-		 MyStacks myStack = new MyStacks();
+		MyStacks myStack = new MyStacks();
 	        myStack.push(70);
 	        myStack.push(30);
 	        myStack.push(56);
