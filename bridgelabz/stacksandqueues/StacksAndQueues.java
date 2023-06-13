a260808c7d62719d5c82d2a5510ddefed98bd9ca
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-13 22:22:01 +0530

    StacksAndQueues.java

--- bridgelabz/stacksandqueues/StacksAndQueues.java
+++ bridgelabz/stacksandqueues/StacksAndQueues.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.stacksandqueues;
 
 public class StacksAndQueues{
-	 public static void main(String[] args) {
+	 public static void main(String[] args)
+	 {
 
 		 MyQueue myQueue = new MyQueue();
 	     myQueue.append(56);
